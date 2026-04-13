2faab01f648735c6d7e2939e854fbf8de38b4acd
Gabriel Fantini <gabrielmarquesfantini@gmail.com>
2026-04-13 15:35:08 -0300

    Update anagrama.java

--- exercicios/TP-02/anagrama.java
+++ exercicios/TP-02/anagrama.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 class anagrama{
     public static void ehAnagrama(String palavra1, String palavra2){
       boolean saoIguais = true;
-        // Verifica se as strings têm o mesmo tamanho, uma condição básica para serem anagramas.
         if (palavra1.length() != palavra2.length()) {
             saoIguais = false;
             MyIO.println("NÃO");
@@ -11,12 +10,10 @@ class anagrama{
         }
 
         if (saoIguais) {
-            // Converte as strings para arrays de caracteres para poder ordená-las.
             char[] array1 = palavra1.toCharArray();
             char[] array2 = palavra2.toCharArray();
             int tam = palavra1.length();
 
-            // Ordena o primeiro array de caracteres em ordem decrescente (Bubble Sort).
             for (int i = 0; i < tam - 1; i++) {
                 for (int j = 0; j < tam - 1 - i; j++) {
                     if (array1[j] < array1[j + 1]) {
@@ -27,7 +24,6 @@ class anagrama{
                 }
             }
 
-            // Ordena o segundo array da mesma forma.
             for (int i = 0; i < tam - 1; i++) {
                 for (int j = 0; j < tam - 1 - i; j++) {
                     if (array2[j] < array2[j + 1]) {
@@ -38,9 +34,7 @@ class anagrama{
                 }
             }
 
-            // Compara os dois arrays ordenados, caractere por caractere.
             for (int i = 0; i < tam; i++) {
-                // Se encontrar uma diferença, não são anagramas e o laço é interrompido.
                 if (array1[i] != array2[i]) {
                     saoIguais = false;
                     MyIO.println("NÃO");
